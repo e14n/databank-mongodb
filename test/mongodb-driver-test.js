16784600598e8a38222704dd339edf3f47ecf61e
Evan Prodromou <evan@status.net>
2012-07-19 22:44:13 -0400

    Test for mongodb driver

---
+++ test/mongodb-driver-test.js
@@ -0,0 +1,29 @@
+// mongodb-driver-test.js
+//
+// Testing the mongodb driver
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require('assert'),
+    vows = require('vows'),
+    databank = require('databank'),
+    Databank = databank.Databank,
+    MongoDatabank = require('../lib/index');
+
+Databank.register('mongodb', MongoDatabank);
+
+var suite = databank.DriverTest('mongodb', {mktmp: true});
+
+suite['export'](module);
